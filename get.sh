a3579c418ea433d767ce36d10c964e934606e6b4
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2018-05-04 02:08:11 -0300

    - Simplificado script instalador

--- get.sh
+++ get.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-# Pegar o PATH do script (https://stackoverflow.com/a/246128/6465636)
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-git clone https://github.com/josenicomaia/LocawebBoilerplate.git LocawebBoilerplate
-$DIR/LocawebBoilerplate/locaweb.sh registrar
-$DIR/LocawebBoilerplate/locaweb.sh ajuda
+php -r "copy('https://github.com/josenicomaia/LocawebBoilerplate/archive/master.zip', 'LocawebBoilerplate-master.zip');"
+unzip LocawebBoilerplate-master.zip
+mv LocawebBoilerplate-master LocawebBoilerplate
+rm LocawebBoilerplate-master.zip
+locaweb.sh registrar
+locaweb.sh ajuda
