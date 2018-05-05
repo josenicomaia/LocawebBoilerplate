2017d979d3a08e4e08950627ef4ce70d8869e585
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2018-05-04 23:47:56 -0300

    - Ajustado get.sh para refletir a realidade da Locaweb

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-php -r "copy('https://github.com/josenicomaia/LocawebBoilerplate/archive/master.zip', 'LocawebBoilerplate-master.zip');" ¨ && \
+php71 -r "copy('https://github.com/josenicomaia/LocawebBoilerplate/archive/master.zip', 'LocawebBoilerplate-master.zip');" ¨ && \
     unzip LocawebBoilerplate-master.zip && \
     mv LocawebBoilerplate-master LocawebBoilerplate && \
     rm LocawebBoilerplate-master.zip && \
-    LocawebBoilerplate/locaweb.sh registrar && \
-    LocawebBoilerplate/locaweb.sh ajuda
+    . LocawebBoilerplate/locaweb.sh registrar && \
+    . LocawebBoilerplate/locaweb.sh ajuda
