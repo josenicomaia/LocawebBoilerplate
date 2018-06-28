d35cbc069b7233cf1da16805e631e7a7df96b982
José Nicodemos Maia Neto <jose+gitlab.com@nicomaia.com.br>
2018-06-27 22:15:51 -0300

    - Colocado prefixo lw_ nas funções pois ao executar ". lw php 7.1" a função php estava sendo executada ao mesmo tempo que a execução do script - Desregistrando variáveis e funções ao fim da execução do script para evitar conflito - Refatorado código para pegar path do script em execução para uma função

--- locaweb.sh
+++ locaweb.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# Pegar o PATH do script (https://stackoverflow.com/a/246128/6465636)
-SOURCE="${BASH_SOURCE[0]}"
+lw_get_script_path() {
+    # Pegar o PATH do script (https://stackoverflow.com/a/246128/6465636)
+    SOURCE="${BASH_SOURCE[0]}"
 
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
 
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    echo $DIR
+}
 
-ajuda() {
-    echo "Uso: locaweb <comando> [pametros...]"
+LW_DIR=$(lw_get_script_path)
+
+lw_ajuda() {
+    echo "Uso: "$(basename $0)" <comando> [pametros...]"
     echo ""
     echo "Comandos:"
-    echo "  php <versao> [env=production|development]:   Faz o ambiente utilizar a versão desejada do PHP."
+    echo "  php <versao=7.1> [env=production|development]:   Faz o ambiente utilizar a versão desejada do PHP."
     echo "    Versoes suportadas:"
     [ -s /usr/bin/php52 ] && echo "     - 5.2"
     [ -s /usr/bin/php53 ] && echo "     - 5.3"
@@ -30,18 +35,18 @@ ajuda() {
     echo "  bash:                                        Instala as configurações padroes do bash (baseado no ubuntu)."
 }
 
-registrar() {
+lw_registrar() {
     if [ ! -d $HOME/bin ]; then
         mkdir -p $HOME/bin
     fi
 
     echo "Registrando LocawebBoilerplate..."
-    ln -s $DIR/locaweb.sh /home/$USER/bin/lw
+    ln -s $LW_DIR/locaweb.sh /home/$USER/bin/lw
 
     echo "Registrado."
 }
 
-instalar_composer() {
+lw_instalar_composer() {
     echo "Baixando composer..."
     command php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 
@@ -59,27 +64,28 @@ instalar_composer() {
     command php -r "unlink('composer-setup.php');"
 }
 
-gerar_chaves_ssh() {
+lw_gerar_chaves_ssh() {
     if [ ! -s $HOME/.ssh/id_rsa ]; then
         echo "Gerando par de chaves do SSH..."
         ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -N ""
 
+        echo ""
         echo "Chave publica:"
         cat $HOME/.ssh/id_rsa.pub
     else
         echo "Já existe um par de chaves gerado."
-
+        echo ""
         echo "Chave publica:"
         cat $HOME/.ssh/id_rsa.pub
     fi
 }
 
-instalar_config_bash() {
+lw_instalar_config_bash() {
     echo "Instalando arquivos de configuracao do Bash..."
-    cp $DIR/.bash_logout $HOME/.bash_logout
-    cp $DIR/.bash_profile $HOME/.bash_profile
-    cp $DIR/.bashrc $HOME/.bashrc
-    cp $DIR/.bash_aliases $HOME/.bash_aliases
+    cp $LW_DIR/.bash_logout $HOME/.bash_logout
+    cp $LW_DIR/.bash_profile $HOME/.bash_profile
+    cp $LW_DIR/.bashrc $HOME/.bashrc
+    cp $LW_DIR/.bash_aliases $HOME/.bash_aliases
 
     echo "Carregando configuracoes..."
     source $HOME/.bash_profile
@@ -87,41 +93,49 @@ instalar_config_bash() {
     echo "Carregado."
 }
 
-php() {
-    PHP_VERSION=$1
-    PHP_ENV=${2:-production}
+lw_php() {
+    PHP_VERSION=${1-"7.1"}
+    PHP_ENV=${2-"production"}
 
     echo "Versão selecionada do PHP: $PHP_VERSION"
-    instalar_config_php $PHP_VERSION $PHP_ENV
+    lw_instalar_config_php $PHP_VERSION $PHP_ENV
 
-    if [ -d $DIR/php/$PHP_VERSION ]; then
+    if [ -d $LW_DIR/php/$PHP_VERSION ]; then
         if [ -f $HOME/bin/php ]; then
             rm $HOME/bin/php
         fi
 
         echo "Registrando versao do PHP para linha de comando (CLI)..."
-        cp $DIR/php/$PHP_VERSION/cli/php$PHP_VERSION.sh $HOME/bin/php
+        cp $LW_DIR/php/$PHP_VERSION/cli/php$PHP_VERSION.sh $HOME/bin/php
         chmod +x $HOME/bin/php
-        cp $DIR/php/$PHP_VERSION/cli/phpize$PHP_VERSION.sh $HOME/bin/phpize
+        cp $LW_DIR/php/$PHP_VERSION/cli/phpize$PHP_VERSION.sh $HOME/bin/phpize
         chmod +x $HOME/bin/phpize
-        cp $DIR/php/$PHP_VERSION/cli/php$PHP_VERSION-config.sh $HOME/bin/php-config
+        cp $LW_DIR/php/$PHP_VERSION/cli/php$PHP_VERSION-config.sh $HOME/bin/php-config
         chmod +x $HOME/bin/php-config
 
+        echo "Fazendo backup do .htaccess em /home/$USER/htacess.bkp..."
+        cp $HOME/public_html/.htaccess $HOME/htaccess.bkp
         echo "Registrando versao do PHP para WEB (CGI)..."
-        sed "s/LOCAWEB_USER/$USER/g" $DIR/php/$PHP_VERSION/.htaccess > $HOME/public_html/.htaccess
-        cp $DIR/php/$PHP_VERSION/cgi/php$PHP_VERSION-cgi.sh $HOME/bin/php-cgi
+        awk '!/AddHandler php[0-9]*-script/' $HOME/public_html/.htaccess | awk '!/suPHP_ConfigPath/' | sed '1{/^[[:space:]]*$/d}' > /tmp/htaccess_corpo
+        sed "s/LOCAWEB_USER/$USER/g" $LW_DIR/php/$PHP_VERSION/.htaccess > /tmp/htaccess
+        echo "" >> /tmp/htaccess
+        echo "" >> /tmp/htaccess
+        cat /tmp/htaccess_corpo >> /tmp/htaccess
+        rm /tmp/htaccess_corpo
+        mv /tmp/htaccess $HOME/public_html/.htaccess
+        cp $LW_DIR/php/$PHP_VERSION/cgi/php$PHP_VERSION-cgi.sh $HOME/bin/php-cgi
         chmod +x $HOME/bin/php-cgi
 
         echo ""
-        command php -v
+command php -v
     else
         echo "Configurações não encontradas!"
     fi
 }
 
-instalar_config_php() {
-    PHP_VERSION=$1
-    PHP_ENV=${2:-production}
+lw_instalar_config_php() {
+    PHP_VERSION=${1-"7.1"}
+    PHP_ENV=${2-"production"}
 
     if [ ! -d $HOME/php/$PHP_VERSION ]; then
         echo "Instalando arquivos de configuracao do PHP..."
@@ -133,36 +147,47 @@ instalar_config_php() {
 
         echo "Copiando configurações do CGI..."
         mkdir -p $HOME/php/$PHP_VERSION/cgi
-        sed "s/LOCAWEB_USER/$USER/g" $DIR/php/$PHP_VERSION/cgi/php.ini-$PHP_ENV > $HOME/php/$PHP_VERSION/cgi/php.ini
+        sed "s/LOCAWEB_USER/$USER/g" $LW_DIR/php/$PHP_VERSION/cgi/php.ini-$PHP_ENV > $HOME/php/$PHP_VERSION/cgi/php.ini
 
         echo "Copiando configurações do CLI..."
         mkdir -p $HOME/php/$PHP_VERSION/cli
-        sed "s/LOCAWEB_USER/$USER/g" $DIR/php/$PHP_VERSION/cli/php.ini-$PHP_ENV > $HOME/php/$PHP_VERSION/cli/php.ini
+        sed "s/LOCAWEB_USER/$USER/g" $LW_DIR/php/$PHP_VERSION/cli/php.ini-$PHP_ENV > $HOME/php/$PHP_VERSION/cli/php.ini
     fi
 }
 
 case "$1" in
     ajuda)
-        ajuda
+        lw_ajuda
     ;;
 
     php)
-        php $2 $3
+        lw_php $2 $3
     ;;
 
     composer)
-        instalar_composer
+        lw_instalar_composer
     ;;
 
     ssh)
-        gerar_chaves_ssh
+        lw_gerar_chaves_ssh
     ;;
 
     registrar)
-        registrar
+        lw_registrar
     ;;
 
     bash)
-        instalar_config_bash
+        lw_instalar_config_bash
     ;;
 esac
+
+# Limpando as variáveis e funções do SCRIPT
+unset LW_DIR
+unset lw_ajuda
+unset lw_instalar_composer
+unset lw_instalar_config_bash
+unset lw_instalar_config_php
+unset lw_gerar_chaves_ssh
+unset lw_php
+unset lw_registrar
+unset lw_get_script_path
